855aa402b24862c291ece8924f90090a8349731a
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-10-28 17:15:29 +0100

    Add validations on model attributes

--- app/models/post.js
+++ app/models/post.js
@@ -1,6 +1,21 @@
 import DS from 'ember-data';
+import { buildValidations, validator } from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const Validations = buildValidations({
+  title: validator('presence', {
+    presence: true
+  }),
+
+  body: validator('presence', {
+    presence: true
+  }),
+
+  tag_ids: validator('presence', {
+    presence: true
+  }),
+});
+
+export default DS.Model.extend(Validations, {
   title: DS.attr('string'),
   body: DS.attr('string'),
   archived: DS.attr('boolean'),
