9fcc4034bf9600bff34c41b49fcd50f9ba45322a
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-10-30 13:30:17 +0100

    Apply validation on selectedTags, add error description

--- app/models/post.js
+++ app/models/post.js
@@ -2,17 +2,19 @@ import DS from 'ember-data';
 import { buildValidations, validator } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  title: validator('presence', {
-    presence: true
-  }),
+  title: {
+    description: 'Title',
+    validators: [validator('presence', true)]
+  },
 
-  body: validator('presence', {
-    presence: true
-  }),
-
-  tag_ids: validator('presence', {
-    presence: true
-  }),
+  body: {
+    description: 'Body',
+    validators: [validator('presence', true)]
+  },
+  selectedTags: {
+    description: 'Tags',
+    validators: [validator('presence', true)]
+  }
 });
 
 export default DS.Model.extend(Validations, {
