012bbc3dd4e06f14ce863600c3c79f06ff815af6
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-10-30 14:51:54 +0100

    Display back-end errors in edit template with extractServerError
    - create extractServerError function
    - remove disabled option from submit button in edit template
    - pass in errors variables to component template
    - change errors display in component template
    - add CP "baseErrors" in edit controller, catch errors when updating a Post

--- app/controllers/posts/edit.js
+++ app/controllers/posts/edit.js
@@ -1,15 +1,32 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import extractServerError from 'draft-app/utils/extract-server-error';
 
 export default Controller.extend({
+  baseErrors: computed('_baseErrors', {
+    get() {
+      return this._baseErrors || [];
+    },
+    set(key, value) {
+      this.set('_baseErrors', value);
+      return this._baseErrors;
+    }
+  }),
+
   actions: {
     async update() {
       let post = this.get('model');
+      this.baseErrors.length = 0;
 
       let selectedTags = post.get('selectedTags');
       post.set('tag_ids', selectedTags.mapBy('id'));
-
-      await post.save();
-      this.transitionToRoute('posts');
+      try {
+        await post.save();
+        this.transitionToRoute('posts');
+      } catch(response) {
+        let errorMessage = extractServerError(response.errors);
+        this.baseErrors.pushObject(errorMessage);
+      }
     }
   }
 });
