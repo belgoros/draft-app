4f050d804a4f915fde6e574d6a07c63fdf02c31d
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-11-02 15:35:03 +0100

    Install S3 deploy pack add-ons

--- config/deploy.js
+++ config/deploy.js
@@ -7,6 +7,10 @@ module.exports = function(deployTarget) {
     // include other plugin configuration that applies to all deploy targets here
   };
 
+  ENV["revision-data"] = {
+    type: 'version-commit'
+  }
+
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
     // configure other plugins for development deploy target here
