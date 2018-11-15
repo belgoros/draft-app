4492f354e5fc2cc1a87f31455224b203d0f10bf2
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-11-15 15:25:56 +0100

    Make post list test pass using qunit-dom

--- tests/acceptance/posts-test.js
+++ tests/acceptance/posts-test.js
@@ -8,10 +8,10 @@ module('Acceptance | posts', function(hooks) {
   setupMirageTest(hooks);
 
   test('Listing posts', async function(assert) {
-    let posts = server.createList('post', 2);
+    this.server.createList('post', 2);
     await visit('/posts');
 
-    assert.equal(this.element.querySelector('td').textContent, posts[0].title);
+    assert.dom('[data-test-post=post-list-item]').exists({ count: 2 }, 'All posts are rendered');
 
   });
 });
